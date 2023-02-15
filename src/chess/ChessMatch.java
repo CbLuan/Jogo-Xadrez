0f5ef63bf6d533df60f920e3cb05d537b7e614a3
Luan <luam10@hotmail.com.br>
2023-02-15 10:42:50 -0300

    Inclusão de movimentação possivel

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,6 +26,12 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.ToPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targePosition) {
 		Position source = sourcePosition.ToPosition();
 		Position target = targePosition.ToPosition();
