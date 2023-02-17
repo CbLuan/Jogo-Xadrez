6a1170028fe1a0457329a7957ecd3cf070ce835b
Luan <luam10@hotmail.com.br>
2023-02-17 11:54:41 -0300

    Contagem de movimento

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -85,7 +85,8 @@ public class ChessMatch {
 	}
 
 	private Piece makeMove(Position source, Position traget) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.IncreaseMoveCount();
 		Piece capturedPiece = board.removePiece(traget);
 		board.placePiece(p, traget);
 
@@ -98,7 +99,8 @@ public class ChessMatch {
 	}
 
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		p.decreaseMoveCount();
 		board.placePiece(p, source);
 
 		if (capturedPiece != null) {
